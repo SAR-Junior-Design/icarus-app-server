3680ac1b86088cb5c89d5aa67b52f23fd55d8fd2
Sam Crane <samcrane8@gmail.com>
2018-08-02 17:50:12 -0500

    added get user test

--- icarus_backend/user/testUserViews.py
+++ icarus_backend/user/testUserViews.py
@@ -16,6 +16,13 @@ register_info = {
         'email': "e2@mail.com"
     }
 
+user_info = {
+    "username": "user1",
+    "email": "e@mail.com",
+    "first_name": "",
+    "last_name": ""
+}
+
 class UserViewTest(TestCase):
 
     def setUp(self):
@@ -42,6 +49,13 @@ class UserViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         user = authenticate(username=register_info['username'], password=register_info['password'])
         self.assertTrue(user)
+
+    def test_get_user(self):
+        response = self.client.post(reverse('icarus login'), json.dumps(login_info), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('get user'))
+        user = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(user, user_info)
 
     def test_is_logged_in(self):
         response = self.client.get(reverse('is logged in'))
