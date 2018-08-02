29205195aa8f764a32bb141f78184f0aed4c8a58
Sam Crane <samcrane8@gmail.com>
2018-08-02 17:45:57 -0500

    fixed conflicts with auth
    my endpoints were the same name as the auth endpoints so I renamed them and got rid of the auth endpoints.

--- icarus_backend/user/UserUrls.py
+++ icarus_backend/user/UserUrls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import UserViews
 
 urlpatterns = [
-    path('login/', UserViews.icarus_login, name='login'),
-    path('logout/', UserViews.icarus_logout, name='logout'),
+    path('login/', UserViews.icarus_login, name='icarus login'),
+    path('logout/', UserViews.icarus_logout, name='icarus logout'),
     path('get/', UserViews.icarus_get_user, name='get user'),
     path('is_logged_in/', UserViews.icarus_is_logged_in, name='is logged in'),
-    path('register_user/', UserViews.icarus_register_user, name='register user')
+    path('register_user/', UserViews.icarus_register_user, name='icarus register user')
 ]
--- icarus_backend/user/testUserViews.py
+++ icarus_backend/user/testUserViews.py
@@ -24,21 +24,21 @@ class UserViewTest(TestCase):
                                         password='12345')
 
     def test_login(self):
-        response = self.client.post(reverse('login'), json.dumps(login_info), content_type='application/json')
+        response = self.client.post(reverse('icarus login'), json.dumps(login_info), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('_auth_user_id', self.client.session)
 
 
     def test_logout(self):
-        response = self.client.post(reverse('login'), json.dumps(login_info), content_type='application/json')
+        response = self.client.post(reverse('icarus login'), json.dumps(login_info), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('_auth_user_id', self.client.session)
-        response = self.client.get(reverse('logout'))
+        response = self.client.get(reverse('icarus logout'))
         self.assertEqual(response.status_code, 200)
         self.assertNotIn('_auth_user_id', self.client.session)
 
     def test_register(self):
-        response = self.client.post(reverse('register user'), json.dumps(register_info), content_type='application/json')
+        response = self.client.post(reverse('icarus register user'), json.dumps(register_info), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         user = authenticate(username=register_info['username'], password=register_info['password'])
         self.assertTrue(user)
@@ -47,5 +47,5 @@ class UserViewTest(TestCase):
         response = self.client.get(reverse('is logged in'))
         self.assertEqual(response.status_code, 200)
         self.assertFalse(json.loads(response.content.decode('utf-8')))
-        response = self.client.post(reverse('login'), json.dumps(login_info), content_type='application/json')
+        response = self.client.post(reverse('icarus login'), json.dumps(login_info), content_type='application/json')
         self.assertTrue(json.loads(response.content.decode('utf-8')))
